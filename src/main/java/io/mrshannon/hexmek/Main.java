0937752b8ac7b67f93ba8b68db1848cf6acf0334
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-10-30 09:06:05 -0600

    Switch to Maven.

---
+++ src/main/java/io/mrshannon/hexmek/Main.java
@@ -0,0 +1,10 @@
+package io.mrshannon.hexmek;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var die = new com.mrshannon.hexmek.Die(6);
+        System.out.println(die);
+	// write your code here
+    }
+}
