2a5329e53cf3d1e9ff57df31026559a43ce07c48
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-11-13 15:44:55 -0700

    Refactor Unit into an interface.

---
+++ src/main/java/io/mrshannon/hexmek/AbstractUnit.java
@@ -0,0 +1,152 @@
+package io.mrshannon.hexmek;
+
+/**
+ * Abstract unit, providing full implementation of movement methods.
+ *
+ * @see io.mrshannon.hexmek.Unit
+ */
+public abstract class AbstractUnit implements Unit {
+
+    private char id;
+    private Hex hex;
+    private Direction facing;
+    private Movement currentMovement;
+    private MovementFactory movementFactory;
+
+    /**
+     * Construct a new unit.
+     *
+     * @param id identification of unit, should be unique to each unit
+     * @param hex starting hex coordinate
+     * @param facing initial facing direction
+     * @param movementFactory factory to use to construct movement strategies
+     */
+    public AbstractUnit(char id, Hex hex, Direction facing, MovementFactory movementFactory) {
+        this.id = id;
+        this.hex = hex;
+        this.facing = facing;
+        this.movementFactory = movementFactory;
+        this.currentMovement = movementFactory.createHalt();
+    }
+
+    @Override
+    public char getId() {
+        return id;
+    }
+
+    @Override
+    public Hex getHex() {
+        return hex;
+    }
+
+    @Override
+    public Direction getFacing() {
+        return facing;
+    }
+
+    @Override
+    public int getMovementPoints() {
+        return currentMovement.getMovementPoints();
+    }
+
+    @Override
+    public int getToHitModifier() {
+        return currentMovement.getToHitModifier();
+    }
+
+    @Override
+    public int getGunneryModifier() {
+        return currentMovement.getGunneryModifier();
+    }
+
+    @Override
+    public void halt() {
+        currentMovement = movementFactory.createHalt();
+    }
+
+    @Override
+    public void cruise() {
+        currentMovement = movementFactory.createCruise();
+    }
+
+    @Override
+    public void flank() {
+        currentMovement = movementFactory.createFlank();
+    }
+
+    @Override
+    public void moveForward() throws MovementPointsExhaustedException {
+        hex = currentMovement.moveForward(hex, facing);
+    }
+
+    @Override
+    public void moveBackward() throws MovementPointsExhaustedException {
+        hex = currentMovement.moveBackward(hex, facing);
+    }
+
+    @Override
+    public void rotateRight() throws MovementPointsExhaustedException {
+        facing = currentMovement.rotateRight(facing);
+    }
+
+    @Override
+    public void rotateLeft() throws MovementPointsExhaustedException {
+        facing = currentMovement.rotateLeft(facing);
+    }
+
+    /**
+     * Save the current location and movement state of the unit.
+     *
+     * <br>
+     *
+     * This does not save damage or ammo state as it is only intended to for use in the movement phase.
+     *
+     * @return a memento containing the current state
+     *
+     * @see AbstractUnit.UnitMemento
+     */
+    @Override
+    public Memento save() {
+        return new UnitMemento(this);
+    }
+
+    /**
+     * Memento to save the location and movement state of a unit.
+     *
+     * <br>
+     *
+     * Does not save damage or ammo state.
+     *
+     * @see io.mrshannon.hexmek.Memento
+     */
+    public class UnitMemento implements Memento {
+
+        private AbstractUnit originator;
+        private Hex hex;
+        private Direction facing;
+        private Movement currentMovement;
+
+        /**
+         * Construct a unit memento.
+         *
+         * @param originator unit to create memento for
+         */
+        private UnitMemento(AbstractUnit originator) {
+            this.originator = originator;
+            this.hex = originator.hex;
+            this.facing = originator.facing;
+            this.currentMovement = (Movement) originator.currentMovement.clone();
+        }
+
+        /**
+         * Restore the original unit to the saved state in the memento.
+         */
+        @Override
+        public void restore() {
+            originator.hex = hex;
+            originator.facing = facing;
+            originator.currentMovement = (Movement) currentMovement.clone();
+        }
+    }
+
+}
--- src/main/java/io/mrshannon/hexmek/Unit.java
+++ src/main/java/io/mrshannon/hexmek/Unit.java
@@ -1,55 +1,32 @@
 package io.mrshannon.hexmek;
 
-public abstract class Unit implements Originator {
-
-    private char id;
-    private Hex hex;
-    private Direction facing;
-    private MovementFactory movementFactory;
-    private Movement currentMovement;
-
-    /**
-     * Construct a new unit.
-     *
-     * @param id identification of unit, should be unique to each unit
-     * @param hex starting hex coordinate
-     * @param facing initial facing direction
-     * @param movementFactory factory to use to construct movement strategies
-     */
-    public Unit(char id, Hex hex, Direction facing, MovementFactory movementFactory) {
-        this.id = id;
-        this.hex = hex;
-        this.facing = facing;
-        this.movementFactory = movementFactory;
-        this.currentMovement = movementFactory.createHalt();
-    }
+/**
+ * Unit interface.
+ *
+ * @see io.mrshannon.hexmek.Originator
+ */
+public interface Unit extends Originator {
 
     /**
      * Get the ID of the unit.
      *
      * @return unique (per game) unit ID
      */
-    public char getId() {
-        return id;
-    }
+    char getId();
 
     /**
      * Get the current hex coordinate of the unit.
      *
      * @return current hex coordinate
      */
-    public Hex getHex() {
-        return hex;
-    }
+    Hex getHex();
 
     /**
      * Get the current facing direction of the unit.
      *
      * @return current facing direction
      */
-    public Direction getFacing() {
-        return facing;
-    }
+    Direction getFacing();
 
     /**
      * Get remaining movement points.
@@ -60,48 +37,44 @@ public abstract class Unit implements Originator {
      *
      * @return remaining movement points
      */
-    public int getMovementPoints() {
-        return currentMovement.getMovementPoints();
-    }
+    int getMovementPoints();
 
     /**
      * Get the current to-hit modifier.
      *
      * @return current to hit modifier
      */
-    public int getToHitModifier() {
-        return currentMovement.getToHitModifier();
-    }
+    int getToHitModifier();
 
     /**
      * Get the current gunnery/attack modifier.
      *
      * @return current attack modifier
      */
-    public int getGunneryModifier() {
-        return currentMovement.getGunneryModifier();
-    }
+    int getGunneryModifier();
+
+
+    /**
+     * Determine if the unit is still alive.
+     *
+     * @return true if the unit is alive
+     */
+    boolean isAlive();
 
     /**
      * Begin halting movement (no movement), resting modifiers and movement points.
      */
-    public void halt() {
-        currentMovement = movementFactory.createHalt();
-    }
+    void halt();
 
     /**
      * Begin cruising movement (slowest, but with backwards movement), resting modifiers and movement points.
      */
-    public void cruise() {
-        currentMovement = movementFactory.createCruise();
-    }
+    void cruise();
 
     /**
      * Begin flanking movement (fastest, no backwards movement), resting modifiers and movement points.
      */
-    public void flank() {
-        currentMovement = movementFactory.createFlank();
-    }
+    void flank();
 
     /**
      * Move unit forward one hexagonal coordinate.
@@ -109,9 +82,7 @@ public abstract class Unit implements Originator {
      * @throws MovementPointsExhaustedException if no movement points remain
      * @throws UnsupportedOperationException if current movement strategy does not support forward movement
      */
-    public void moveForward() throws MovementPointsExhaustedException {
-        hex = currentMovement.moveForward(hex, facing);
-    }
+    void moveForward() throws MovementPointsExhaustedException;
 
     /**
      * Move unit backward one hexagonal coordinate.
@@ -119,9 +90,7 @@ public abstract class Unit implements Originator {
      * @throws MovementPointsExhaustedException if no movement points remain
      * @throws UnsupportedOperationException if current movement strategy does not support backward movement
      */
-    public void moveBackward() throws MovementPointsExhaustedException {
-        hex = currentMovement.moveBackward(hex, facing);
-    }
+    void moveBackward() throws MovementPointsExhaustedException;
 
     /**
      * Rotate unit right (1 direction).
@@ -129,9 +98,7 @@ public abstract class Unit implements Originator {
      * @throws MovementPointsExhaustedException if no movement points remain
      * @throws UnsupportedOperationException if current movement strategy does not support rotation to the right
      */
-    public void rotateRight() throws MovementPointsExhaustedException {
-        facing = currentMovement.rotateRight(facing);
-    }
+    void rotateRight() throws MovementPointsExhaustedException;
 
     /**
      * Rotate unit right (1 direction).
@@ -139,63 +106,6 @@ public abstract class Unit implements Originator {
      * @throws MovementPointsExhaustedException if no movement points remain
      * @throws UnsupportedOperationException if current movement strategy does not support rotation to the left
      */
-    public void rotateLeft() throws MovementPointsExhaustedException {
-        facing = currentMovement.rotateLeft(facing);
-    }
-
-    /**
-     * Save the current location and movement state of the unit.
-     *
-     * <br>
-     *
-     * This does not save damage or ammo state as it is only intended to for use in the movement phase.
-     *
-     * @return a memento containing the current state
-     *
-     * @see io.mrshannon.hexmek.Unit.UnitMemento
-     */
-    @Override
-    public Memento save() {
-        return new UnitMemento(this);
-    }
-
-    /**
-     * Memento to save the location and movement state of a unit.
-     *
-     * <br>
-     *
-     * Does not save damage or ammo state.
-     *
-     * @see io.mrshannon.hexmek.Memento
-     */
-    public class UnitMemento implements Memento {
-
-        private Unit originator;
-        private Hex hex;
-        private Direction facing;
-        private Movement currentMovement;
-
-        /**
-         * Construct a unit memento.
-         *
-         * @param originator unit to create memento for
-         */
-        private UnitMemento(Unit originator) {
-            this.originator = originator;
-            this.hex = originator.hex;
-            this.facing = originator.facing;
-            this.currentMovement = (Movement) originator.currentMovement.clone();
-        }
-
-        /**
-         * Restore the original unit to the saved state in the memento.
-         */
-        @Override
-        public void restore() {
-            originator.hex = hex;
-            originator.facing = facing;
-            originator.currentMovement = (Movement) currentMovement.clone();
-        }
-    }
+    void rotateLeft() throws MovementPointsExhaustedException;
 
 }
