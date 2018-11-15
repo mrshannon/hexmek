0d24597437f04d31f57fcde380d0956b4d649aa1
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-11-14 23:45:17 -0700

    Begin working on the map view.

--- src/main/java/io/mrshannon/hexmek/Main.java
+++ src/main/java/io/mrshannon/hexmek/Main.java
@@ -5,26 +5,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        var die = new io.mrshannon.hexmek.Die(12);
-        var dice = new DiceRoller(new Die(6), new Die(6));
-        for (int i = 0; i <= 20; ++i) {
-            System.out.println(String.format("%d = %f", i, dice.probability(i)));
-        }
-        Direction d = new North();
-        for (int i = 0; i <= 20; i++) {
-            System.out.println(d);
-            d = d.rotateRight();
-        }
-        for (int i = 0; i <= 20; i++) {
-            System.out.println(d);
-            d = d.rotateLeft();
-        }
-
-        var loader = new MapLoader("default");
-        HexMap map;
         try {
-            map = loader.createMap();
-            System.out.println(map.getTile(new Hex(5,13)));
+            var map = (new MapLoader("default")).createMap();
+            var view = new MapView(map);
+            view.render();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
---
+++ src/main/java/io/mrshannon/hexmek/View.java
@@ -0,0 +1,13 @@
+package io.mrshannon.hexmek;
+
+/**
+ * Basic view interface.
+ */
+public interface View {
+
+    /**
+     * Render the view.
+     */
+    void render();
+
+}
