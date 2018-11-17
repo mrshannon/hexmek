dafb2ae00ac4816ade89f634232dc3396f662795
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-11-16 23:53:40 -0700

    Add Vehicle class.

---
+++ src/main/java/io/mrshannon/hexmek/Vehicle.java
@@ -0,0 +1,144 @@
+package io.mrshannon.hexmek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A ground vehicle unit.
+ */
+public class Vehicle extends AbstractUnit {
+
+    private DiceRoller dice;
+    private RealComponent turret;
+    private RealComponent front;
+    private RealComponent rear;
+    private RealComponent leftSide;
+    private RealComponent rightSide;
+
+    /**
+     * Construct a new vehicle.
+     *
+     * @param id              identification of unit, should be unique to each unit
+     * @param hex             starting hex coordinate
+     * @param facing          initial facing direction
+     * @param movementFactory factory to use to construct movement strategies
+     * @param turret          turret component
+     * @param front           front component
+     * @param rear            rear component
+     * @param leftSide        left side component
+     * @param rightSide       right side component
+     */
+    public Vehicle(char id, Hex hex, Direction facing, MovementFactory movementFactory,
+                   RealComponent turret, RealComponent front, RealComponent rear,
+                   RealComponent leftSide, RealComponent rightSide) {
+        super(id, hex, facing, movementFactory);
+        this.turret = turret;
+        this.front = front;
+        this.rear = rear;
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+        this.dice = new DiceRoller(new Die(6), new Die(6));
+    }
+
+    /**
+     * Get the amount of turret armour.
+     *
+     * @return turret armour points
+     */
+    public int getTurretArmour() {
+        return turret.getArmour();
+    }
+
+    /**
+     * Get the amount of front armour.
+     *
+     * @return turret front points
+     */
+    public int getFrontArmour() {
+        return front.getArmour();
+    }
+
+    /**
+     * Get the amount of rear armour.
+     *
+     * @return turret rear points
+     */
+    public int getRearArmour() {
+        return rear.getArmour();
+    }
+
+    /**
+     * Get the amount of left side armour.
+     *
+     * @return turret left side points
+     */
+    public int getLeftSideArmour() {
+        return leftSide.getArmour();
+    }
+
+    /**
+     * Get the amount of right side armour.
+     *
+     * @return turret right side points
+     */
+    public int getRightSideArmour() {
+        return rightSide.getArmour();
+    }
+
+    @Override
+    public List<Component> getComponents() {
+        var components = new ArrayList<Component>();
+        components.add(turret);
+        components.add(front);
+        components.add(rear);
+        components.add(leftSide);
+        components.add(rightSide);
+        return components;
+    }
+
+    /**
+     * Apply damage to a random component of the unit.
+     *
+     * @param weapon the weapon that is doing the damage
+     * @param damage amount of damage to apply
+     * @return list of damage records
+     */
+    @Override
+    public List<DamageRecord> applyDamage(Weapon weapon, int damage) {
+        dice.roll();
+        switch (dice.getTotal()) {
+            case 2:
+                return rear.applyDamage(weapon, damage);
+            case 3:
+                damageMobility();
+                return front.applyDamage(weapon, damage);
+            case 4:
+                damageMobility();
+                return front.applyDamage(weapon, damage);
+            case 5:
+                damageMobility();
+                return rightSide.applyDamage(weapon, damage);
+            case 6:
+                return front.applyDamage(weapon, damage);
+            case 7:
+                return front.applyDamage(weapon, damage);
+            case 8:
+                return front.applyDamage(weapon, damage);
+            case 9:
+                damageMobility();
+                return leftSide.applyDamage(weapon, damage);
+            case 10:
+                return turret.applyDamage(weapon, damage);
+            case 11:
+                return turret.applyDamage(weapon, damage);
+            case 12:
+                return rear.applyDamage(weapon, damage);
+        }
+        throw new RuntimeException("The code is broken!");
+    }
+
+    @Override
+    public boolean isDestroyed() {
+        return false;
+    }
+}
