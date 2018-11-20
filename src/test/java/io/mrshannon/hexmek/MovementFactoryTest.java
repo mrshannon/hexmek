92e512caad486cb974b939f99fd3a6d2d488d753
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-11-20 14:10:49 -0700

    Update MovementFactroy unit tests.

--- src/test/java/io/mrshannon/hexmek/MovementFactoryTest.java
+++ src/test/java/io/mrshannon/hexmek/MovementFactoryTest.java
@@ -52,4 +52,44 @@ public class MovementFactoryTest {
         assertThat(movement35, instanceOf(Flank.class));
         assertThat(movement46, instanceOf(Flank.class));
     }
+
+    @Test
+    public void getCruiseMovementPoints() {
+        assertEquals(3, factory35.getCruiseMovementPoints());
+        assertEquals(4, factory46.getCruiseMovementPoints());
+    }
+
+    @Test
+    public void getFlankMovementPoints() {
+        assertEquals(5, factory35.getFlankMovementPoints());
+        assertEquals(6, factory46.getFlankMovementPoints());
+    }
+
+    @Test
+    public void damageMobility() {
+        factory35.damageMobility();
+        assertEquals(2, factory35.getCruiseMovementPoints());
+        assertEquals(3, factory35.getFlankMovementPoints());
+        factory35.damageMobility();
+        assertEquals(1, factory35.getCruiseMovementPoints());
+        assertEquals(2, factory35.getFlankMovementPoints());
+        factory35.damageMobility();
+        assertEquals(0, factory35.getCruiseMovementPoints());
+        assertEquals(0, factory35.getFlankMovementPoints());
+
+        factory46.damageMobility();
+        assertEquals(3, factory46.getCruiseMovementPoints());
+        assertEquals(5, factory46.getFlankMovementPoints());
+    }
+
+    @Test
+    public void destroyMobility() {
+        factory35.destroyMobility();
+        assertEquals(0, factory35.getCruiseMovementPoints());
+        assertEquals(0, factory35.getFlankMovementPoints());
+
+        factory46.destroyMobility();
+        assertEquals(0, factory46.getCruiseMovementPoints());
+        assertEquals(0, factory46.getFlankMovementPoints());
+    }
 }
