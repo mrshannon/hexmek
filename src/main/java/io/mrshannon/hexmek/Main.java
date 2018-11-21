cf70b3d2479c6fd02d2d76d0aea4d61798322b15
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-11-21 03:41:16 -0700

    Complete the MapView class.

--- src/main/java/io/mrshannon/hexmek/Main.java
+++ src/main/java/io/mrshannon/hexmek/Main.java
@@ -1,5 +1,7 @@
 package io.mrshannon.hexmek;
 
+import io.mrshannon.hexmek.views.MapView;
+
 import java.io.IOException;
 
 public class Main {
@@ -8,6 +10,9 @@ public class Main {
         try {
             var map = (new MapLoader("default")).createMap();
             var view = new MapView(map);
+            view.addUnit(UnitFactory.createUnit("Hunchback HBK-4G", map, new Hex(4, 4), new SouthEast()));
+            view.addUnit(UnitFactory.createUnit("Manticore Heavy Tank", map, new Hex(7, 12), new North()));
+            view.addUnit(UnitFactory.createUnit("Scorpion Light Tank", map, new Hex(15, 17), new SouthEast()));
             view.render();
         } catch (IOException e) {
             System.out.println(e.getMessage());
---
+++ src/main/java/io/mrshannon/hexmek/views/MapView.java
@@ -0,0 +1,132 @@
+package io.mrshannon.hexmek.views;
+
+import io.mrshannon.hexmek.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MapView implements View {
+
+    private HexMap map;
+    private ArrayList<Unit> units;
+
+    public MapView(HexMap map) {
+        this.map = map;
+        this.units = new ArrayList<>();
+    }
+
+    public MapView(HexMap map, Collection<Unit> units) {
+        this(map);
+        this.units = new ArrayList<>(units);
+    }
+
+    public void addUnit(Unit unit) {
+        units.add(unit);
+    }
+
+    @Override
+    public void render() {
+        printColumnHeadings();
+        printTop();
+        for (int row = 1; row < map.getHeight(); ++row) {
+            printRow(row, false);
+        }
+        printRow(map.getHeight(), true);
+    }
+
+    private void printColumnHeadings() {
+        System.out.print("     ");
+        for (int i = 1; i <= map.getWidth(); ++i) {
+            System.out.print(String.format(" %2d", i));
+        }
+        System.out.print("\n");
+    }
+
+    private void printTop() {
+        System.out.print("     ");
+        for (int i = 1; i <= map.getWidth()/2; ++i) {
+            System.out.print(" __   ");
+        }
+        if ((map.getWidth() & 1) == 1) {
+            System.out.print(" __");
+        }
+        System.out.print("\n");
+    }
+
+    private char getTerrainChar(Hex hex) {
+        var tile = map.getTile(hex);
+        switch (tile.getId()) {
+            case "clear:1":
+                return ' ';
+            case "woods:1":
+                return '*';
+            case "woods:2":
+                return '#';
+            default:
+                return ' ';
+        }
+    }
+
+    private char getUnitIdChar(Hex hex) {
+        for (var unit : units) {
+            if (unit.getHex().equals(hex)) {
+                return unit.getId();
+            }
+        }
+        return '_';
+    }
+
+    private char getUnitDirectionChar(Hex hex) {
+        for (var unit : units) {
+            if (unit.getHex().equals(hex)) {
+                if (unit.getFacing() instanceof North) {
+                    return '0';
+                } else if (unit.getFacing() instanceof NorthEast) {
+                    return '1';
+                } else if (unit.getFacing() instanceof SouthEast) {
+                    return '2';
+                } else if (unit.getFacing() instanceof South) {
+                    return '3';
+                } else if (unit.getFacing() instanceof SouthWest) {
+                    return '4';
+                } else { // NorthWest
+                    return '5';
+                }
+            }
+        }
+        return '_';
+    }
+
+    private void printRow(int row, boolean lastLine) {
+        System.out.print(String.format("  %2d ", row));
+        for (int column = 1; column <= map.getWidth(); ++column) {
+            Hex hex = new Hex(column, row);
+            if ((column & 1) == 1) { // odd
+                char c = getTerrainChar(hex);
+                System.out.print(String.format("/%c%c\\", c, c));
+            } else { // even
+                if (lastLine) {
+                    System.out.print("__");
+                } else {
+                    System.out.print(String.format("%c%c", getUnitIdChar(hex), getUnitDirectionChar(hex)));
+                }
+            }
+        }
+        System.out.print("\n     ");
+        for (int column = 1; column <= map.getWidth(); ++column) {
+            Hex hex = new Hex(column, row);
+            if ((column & 1) == 1) { // odd
+                System.out.print(String.format("\\%c%c/", getUnitIdChar(hex), getUnitDirectionChar(hex)));
+            } else { // even
+                if (lastLine) {
+                    System.out.print("  ");
+                } else {
+                    char c = getTerrainChar(new Hex(column, row));
+                    System.out.print(String.format("%c%c", c, c));
+                }
+            }
+        }
+        System.out.print("\n");
+    }
+
+}
